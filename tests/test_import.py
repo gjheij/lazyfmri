6650abf21ece8f76188c0886adaa1cb0fddad807
gjheij <j.heij@spinozacentre.nl>
2024-12-03 15:19:51 +0100

    simple import

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,11 +1,6 @@
 import nideconv as nd
-from . import plotting
 import pandas as pd
 import os
 opj = os.path.join
 opd = os.path.dirname
 
-beta_file = opj(opd(opd(plotting.__file__)), "data", "betas.csv")
-profile_file = opj(opd(opd(plotting.__file__)), "data", "betas.csv")
-df_beta = pd.read_csv(beta_file)
-df_prof = pd.read_csv(profile_file)
