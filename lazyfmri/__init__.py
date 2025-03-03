0427f3e343f5f37b8e84206b5e8e911f146ece9d
gjheij <j.heij@spinozacentre.nl>
2025-03-03 16:41:04 +0100

    import

--- lazyfmri/__init__.py
+++ lazyfmri/__init__.py
@@ -1,3 +1,10 @@
-# __init__.py
+# lazyfmri/__init__.py
 
-__version__ = "0.1.0"
+# Now import core functionality (optional, but typical in __init__.py)
+from . import utils
+from . import plotting
+from . import dataset
+from . import fitting
+from . import preproc
+
+__all__ = ["utils", "plotting", "dataset", "fitting", "preproc"]
